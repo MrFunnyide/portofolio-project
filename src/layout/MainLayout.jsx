443da5d518c5a4051352cad3059d2d7c44730875
ajibayu <ajineo3@gmail.com>
2024-07-28 15:32:18 +0700

    perbaikan dikit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 import { Outlet } from 'react-router-dom';
 const MainLayout = () => {
   return (
-    <div className="max-w-full lg:max-w-6xl mx-auto px-2 py-4 ">
+    <div className="max-w-7xl lg:max-w-6xl mx-auto px-2 py-4 ">
       <Navbar />
       <Outlet />
     </div>
