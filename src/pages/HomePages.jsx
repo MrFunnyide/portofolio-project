70bcca1ea78901ed6ccdda19b836380e94f48c2e
ajibayu <ajineo3@gmail.com>
2024-07-29 11:50:20 +0700

    add magic ui

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import TypingAnimation from "@/components/magicui/typing-animation";
 
 const HomePages = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="text-center">
-        <h1 className="text-6xl">Web Developer Enthusiast</h1>
+        <TypingAnimation text="Web Developer Enthusiast" className="text-6xl" />
+        {/* <h1 className="text-6xl">Web Developer Enthusiast</h1> */}
         <p className="my-4 font-medium">Aji Bayu Permadi</p>
         <NavLink
           to="/"
-          className="font-semibold px-8 py-2 text-sm bg-slate-500 hover:bg-slate-100 hover:text-black hover:border hover:border-slate-500 text-white rounded-md">
+          className="font-semibold px-8 py-2 text-sm bg-slate-500 hover:bg-slate-100 hover:text-black hover:border hover:border-slate-500 text-white rounded-md"
+        >
           My CV
         </NavLink>
       </div>
